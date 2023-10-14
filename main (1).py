e58df9deb41bd91c560d7b972d15efe881cf8461
Nandhini-2005 <144894047+Nandhini-2005@users.noreply.github.com>
2023-10-14 11:16:53 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,9 +1,10 @@
-def fact(n):
-  if n == 0:
-    return 1
-  else:
-    return n * fact(n - 1)
+year =int(input("enter a year:"))
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
 
 
-print(fact(0))
-print(fact(5))
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+else:
+    print("{0} is not a leap year".format(year))
